d961cdd68821ea68eed355514fc2253f07f52c83
Fisher <yverrr@gmail.com>
2022-07-19 14:56:31 +0800

    rewrite problem1 from python to c++

---
+++ problem1.cpp
@@ -0,0 +1,26 @@
+/**
+Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+#include <cstdio>
+
+int multiples_3_5(int n):
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            res += i;
+    }
+    return res;
+
+
+int main()
+{
+    int n = 1000;
+    int result = multiples_3_5(n);
+    printf("The sum of all the multiples of 3 or 5 below %d is %d", n, result);
+}
